50b3b6d91218ab9e39ca7a274665d0220ab8453c
Ahmed Zulfiqar <ahmedzulfiqar.dev@gmail.com>
2026-04-23 22:13:57 +0500

    Config: Updated site URL for correct sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://orthodonticsalign.com',
+  site: 'https://validmvps.vercel.app',
   vite: {
     plugins: [tailwindcss()],
     define: {
